ad952c77d331f2f98cefd7e33fd71f030cf35dd6
Scott Nguyen <scottqmn@gmail.com>
2021-08-11 09:15:57 -0700

    post list material icons

--- components/PostList/index.jsx
+++ components/PostList/index.jsx
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
 import { Link, RichText } from '../Prismic'
 import styles from './styles.module.scss'
 
 const PostList = ({ posts }) => {
     return (
-        <ul>
+        <ul className={styles.list}>
             {posts.map((post) => (
-                <li key={post.uid} className='t-logo'>
+                <li key={post.uid} className={clsx(styles.item, 't-logo')}>
                     <Link document={post}>
                         <RichText content={post.data.title} asText />
+                        <ArrowForwardIcon />
                     </Link>
                 </li>
             ))}
