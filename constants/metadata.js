9ef13df26e8df5dafc98749086430d94352e7e48
Scott Nguyen <scottqmn@gmail.com>
2021-08-14 11:51:47 -0700

    update metadata

--- constants/metadata.js
+++
@@ -1,6 +0,0 @@
-export default {
-    title: 'Needless Info',
-    description: 'Personal blog under the guise of advice',
-    image: '/metadata/meta_image.jpg',
-    url: 'https://needless.info',
-}
